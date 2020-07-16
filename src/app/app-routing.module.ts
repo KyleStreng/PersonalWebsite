ed99d601447e05cd110a0c4287da716aafdab248
Kyle Streng <kylewstreng@gmail.com>
2020-07-15 21:17:26 -0500

    Removed unneeded import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about/about.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { ResumeComponent } from './resume/resume.component';
