8316208cdd6150d179e19dc48d6bd19d30662b54
Kyle Streng <kylewstreng@gmail.com>
2020-08-02 12:56:06 -0500

    Added smooth scrolling, finished education component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
-import { ResumeComponent } from './resume/resume.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ResumeComponent,
     SideBarComponent,
-    ExperienceComponent
+    ExperienceComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
